606e3729ae78bebaebcb2ce0af78727abd5c1bad
Miguel Ángel Soto <miguelangelsoto@rudo.es>
2024-04-05 18:42:36 +0200

    Initial Commit

---
+++ SpatialDex/SpatialDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialDexApp.swift
+//  SpatialDex
+//
+//  Created by Miguel Ángel Soto González on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
